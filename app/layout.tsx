3782bdffe5b92da9ac4e94af5fc27f5b979a3e76
info@escaperoom-milano.comAGEProject1313 <info@escaperoom-milano.com>
2026-05-12 22:43:31 +0200

    Add mobile viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.kairosdetectives.com"),
 
